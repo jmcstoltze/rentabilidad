b24ca0b0e4d5880ef7b0ec4af4847e8cae9c30a7
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-02-16 19:24:34 -0300

    Correcion aritmetica

--- emprendedor2.py
+++ emprendedor2.py
@@ -1,12 +1,12 @@
 #instrucciones
-print("ATENCIÓN!! Los valores que se ingresen deben estar en pesos chilenos (números enteros)")
+print("ATENCIÓN!! Los valores monetarios a ingresar deben ser numéricos")
 print("")
 
 #usuario ingresa valor suscripción
 precio_normal = input('Ingrese el precio de la suscripción para usuarios normales en pesos: $ ')
 
-#se calcula el valor suscripción usuario premium y se imprime en pantalla
-precio_premium = int(precio_normal)*2
+#se calcula el valor suscripción usuario premium (sumándole 50%) y se imprime en pantalla
+precio_premium = int(precio_normal)*1.5
 print(f"La suscripción para usuarios premium será de: $ {precio_premium}")
 
 #usuario ingresa cantidad 1
@@ -17,8 +17,8 @@ usuarios_premium = input('Ingrese la cantidad de usuarios premium: ')
 #usuario ingresa gastos totales
 gastos_totales = input('Ingrese gastos totales en pesos: $ ')
 
-#se calcula el resultado esperado
-utilidades = (int(precio_normal)*int(usuarios_normales)) + (int(precio_premium)*int(usuarios_premium)) - int(gastos_totales)
+#se calcula el resultado esperado redondeando 2 decimales
+utilidades = round((float(precio_normal)*float(usuarios_normales)) + (float(precio_premium)*float(usuarios_premium)) - float(gastos_totales),2)
 
 #resultado se imprime en pantalla
 print(f"Las utilidades en pesos son: $ {utilidades}")
